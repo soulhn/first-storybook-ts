5b8f5d7cf940a4114cc3268e072c0b2801fc0252
soulhn <vldnrlf123@naver.com>
2023-06-04 05:59:40 +0900

    feat: 버튼 컴포넌트 리팩토링 + 블로깅

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
 import React, { useCallback } from "react";
 import CustomButton from "./components/CustomButton";
 
+// 버전2 useCallBack 사용
+function App() {
+  const handleClick = useCallback(() => {
+    console.log("버튼을 클릭합니다.");
+  }, []); // 의존성 배열이 비어 있으므로, handleClick은 컴포넌트가 처음 렌더링될 때 한 번만 생성
+  return (
+    <div className="App">
+      <CustomButton buttonText="Click me!" backgroundColor="blue" color="white" clickHandler={handleClick} />
+    </div>
+  );
+}
+
+export default App;
+
 // import CustomButton, { ButtonProps } from "./components/CustomButton";
 // 버전1
 // function App() {
@@ -20,17 +34,3 @@ import CustomButton from "./components/CustomButton";
 //     </div>
 //   );
 // }
-
-// 버전2 useCallBack 사용
-function App() {
-  const handleClick = useCallback(() => {
-    console.log("버튼을 클릭합니다.");
-  }, []); // 의존성 배열이 비어 있으므로, handleClick은 컴포넌트가 처음 렌더링될 때 한 번만 생성
-  return (
-    <div className="App">
-      <CustomButton buttonText="Click me!" backgroundColor="blue" color="white" clickHandler={handleClick} />
-    </div>
-  );
-}
-
-export default App;
